968a994f25a180a791d3120643cdb6c73171c167
sanvirk99 <sanseerat99@gmail.com>
2024-08-24 10:40:42 -0700

    login and logout, condtional render

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,88 @@ import Blog from './components/Blog'
 import blogService from './services/blogs'
 import loginServices from './services/login'
 
+
+const App = () => {
+  
+  const [user, setUser] = useState(null)
+
+
+  useEffect(() => {
+
+    const userStr = window.localStorage.getItem('user')
+    if(userStr){
+      setUser(JSON.parse(userStr))
+    }
+
+
+  },[])
+
+  const loginAtempt = async (username, password) => {
+
+      //if success populate user
+      console.log(`request login ${username} `)
+      try {
+        const user=await loginServices.login({username,password})
+        setUser(user)
+        window.localStorage.setItem('user',JSON.stringify(user))
+      }catch (exception){
+        console.log('invalid credentials')
+        setUser(null)
+        window.localStorage.removeItem('user')
+      }
+  }
+
+  const logout = () => {
+
+    if(user !== null){
+      setUser(null)
+    }
+
+  }
+
+  return (
+    <div>
+
+
+      <LoginForm onLogin={loginAtempt} onLogout={logout} user={user}/>
+      <Blogs user={user} />
+      
+    </div>
+  )
+}
+
+//pass user as prop
+const Blogs = (props) => {
+  const [blogs, setBlogs] = useState([])
+
+
+  useEffect(() => {
+
+    if(props.user !== null){
+      blogService.getAll().then((blogs) => {
+        setBlogs(blogs)
+      })
+    }},[props.user])
+
+  return (
+    <>
+    {props.user !== null && (
+      <div>
+      <h2>blogs</h2>
+      {blogs.map(blog => (
+        <Blog key={blog.id} blog={blog} />
+      ))}
+      </div>
+      
+    )}
+    </>
+  )
+    
+} 
+
+
+
 const LoginForm = (props) => {
-
-
 
   const [username, setUsername] = useState('')  
   const [password, setPassword] = useState('')
@@ -21,12 +100,19 @@ const LoginForm = (props) => {
   const requestLogin =  async (e) => {
     e.preventDefault()
     console.log(e)
-    await props.onLogin(username,password)
+    props.onLogin(username,password) //dont need to await here
+  }
+
+  const requestLogout = (e) => {
+
+    props.onLogout()
   }
 
   return (
-
-    <form onSubmit={requestLogin}>
+    <>
+    
+    {props.user === null && (
+      <form onSubmit={requestLogin}>
       <div>
         <label>Username :</label>
         <input type='text' value={username} onChange={userChange} />
@@ -36,46 +122,18 @@ const LoginForm = (props) => {
         <button type="submit">Login</button>
       </div>
     </form>
+    )}
 
-  )
-
-
-}
-
-const App = () => {
-  const [blogs, setBlogs] = useState([])
-  
-  const [user, setUser] = useState(null)
-
-  const loginAtempt = async (username, password) => {
-
-      //if success populate user
-      console.log(`request login ${username} `)
-
-      const res=await loginServices.login({username,password})
-
-      console.log('response')
-
-
-  }
-
-
-  useEffect(() => {
-    blogService.getAll().then((blogs) => {
-      setBlogs(blogs)
-    }
-    )  
-  }, [])
-
-  return (
-    <div>
-      
-      <LoginForm onLogin={loginAtempt}/>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
-    </div>
+    {props.user !== null && (
+      <div>
+        <label>{props.user.name} logged in</label>
+        <button type="submit" onClick={requestLogout}>Logout</button>
+      </div>
+    )}
+    
+    
+    </>
+    
   )
 }
 
